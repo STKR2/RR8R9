2804defcd27333ee58703beac7cde42c85ee0b18
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-02-02 09:22:56 +0530

    Create Faker.py

---
+++ AarohiX/plugins/Management/Faker.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from faker import Faker
+from AarohiX import app
+
+fake = Faker()
+
+@app.on_message(filters.command("rand"))
+def generate_info(client, message):
+    name = fake.name()
+    address = fake.address()
+    country = fake.country()
+    phone_number = fake.phone_number()
+    email = fake.email()
+    city = fake.city()
+    state = fake.state()
+    zipcode = fake.zipcode()
+
+    info_message = (
+        f"**Full Name:** {name}\n"
+        f"**Address:** {address}\n"
+        f"**Country:** {country}\n"
+        f"**Phone Number:** {phone_number}\n"
+        f"**Email:** {email}\n"
+        f"**City:** {city}\n"
+        f"**State:** {state}\n"
+        f"**zipcode:** {zipcode}"
+    )
+
+    message.reply_text(info_message)
