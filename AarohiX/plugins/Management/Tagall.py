2e2c62e11bdc1d337840b428abd17105fb46e6c3
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-02-02 22:02:38 +0530

    Update Tagall.py

--- AarohiX/plugins/Management/Tagall.py
+++ AarohiX/plugins/Management/Tagall.py
@@ -43,7 +43,7 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break 
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f" [{m.user.first_name}](tg://user?id={m.user.id})"
             if usernum == 5:
                 await app.send_message(message.chat.id,f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
