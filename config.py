9306ecde857aadab705bcc85bbb350d106dfeff3
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-02-02 10:18:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,6 +25,8 @@ LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
 OWNER_ID = int(getenv("OWNER_ID", "2107529793"))
 
+RUN = list(map(int, getenv("RUN", "2107529793 5618845741").split()))
+
 BOT_USERNAME = getenv("BOT_USERNAME" , "Alone_Dil_bot")
 
 COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
