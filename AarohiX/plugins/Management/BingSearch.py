98253ba4163755b85c23a2937c68e7ac5555e01d
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-02-02 09:20:20 +0530

    Create BingSearch.py

---
+++ AarohiX/plugins/Management/BingSearch.py
@@ -0,0 +1,33 @@
+from AarohiX import app 
+import requests as r
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from pyrogram import filters 
+
+API_URL = "https://sugoi-api.vercel.app/search"
+
+@app.on_message(filters.command("bingsearch"))
+async def bing_search(dilop, message):
+    try:
+        if len(message.command) == 1:
+            await message.reply_text("Please provide a keyword to search.")
+            return
+
+        keyword = " ".join(message.command[1:])
+        params = {"keyword": keyword}
+        response = r.get(API_URL, params=params)
+
+        if response.status_code == 200:
+            results = response.json()
+            if not results:
+                await message.reply_text("No results found.")
+            else:
+                message_text = ""
+                for result in results[:7]:
+                    title = result.get("title", "")
+                    link = result.get("link", "")
+                    message_text += f"{title}\n{link}\n\n"
+                await message.reply_text(message_text.strip())
+        else:
+            await message.reply_text("Sorry, something went wrong with the search.")
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {str(e)}")
