d3769e5c0e3c260a18ef4372cc310bbad8f3c788
sᴀɢᴀʀˣᴀᴅɪᴛɪ <112709751+stkeditz@users.noreply.github.com>
2024-02-04 16:47:42 +0530

    Update inline.py

--- AarohiX/plugins/bot/inline.py
+++ AarohiX/plugins/bot/inline.py
@@ -6,7 +6,7 @@ from pyrogram.types import (
 from youtubesearchpython.__future__ import VideosSearch
 
 from AarohiX import app
-from AarohiX.utils.inlinequery import answer
+#from AarohiX.utils.inlinequery import answer
 from config import BANNED_USERS
 
 
