b4197bbc4be87192b5e37c179761aca89c11525f
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-17 22:45:01 +0300

    Update play.py

--- AarohiX/utils/inline/play.py
+++ AarohiX/utils/inline/play.py
@@ -20,10 +20,10 @@ def track_markup(_, videoid, user_id, channel, fplay):
         ],
         [
             InlineKeyboardButton(
-                text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
+                text="「 الدعم 」", url=SUPPORT_CHAT,
             )
         ],
         [
@@ -37,30 +37,6 @@ def track_markup(_, videoid, user_id, channel, fplay):
 
 
 def stream_markup_timer(_, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "♡╌╌╌╌╌╌╌╌"
-    elif 10 < umm < 20:
-        bar = "╌♡╌╌╌╌╌╌╌"
-    elif 20 <= umm < 30:
-        bar = "╌╌♡╌╌╌╌╌╌"
-    elif 30 <= umm < 40:
-        bar = "╌╌╌♡╌╌╌╌╌"
-    elif 40 <= umm < 50:
-        bar = "╌╌╌╌♡╌╌╌╌"
-    elif 50 <= umm < 60:
-        bar = "╌╌╌╌╌♡╌╌╌"
-    elif 60 <= umm < 70:
-        bar = "╌╌╌╌╌╌♡╌╌"
-    elif 70 <= umm < 80:
-        bar = "╌╌╌╌╌╌╌♡╌"
-    elif 80 <= umm < 95:
-        bar = "╌╌╌╌╌╌╌╌♡╌"
-    else:
-        bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
         [
             InlineKeyboardButton(
@@ -69,15 +45,15 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‹ استمرار ›", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="‹ مؤقتا ›", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="‹ ايقاف ›", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="🥰ʀᴇᴘʟᴀʏ[↻]🥰", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(
-                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
+                text="「 الدعم 」", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -88,18 +64,18 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}")
+            InlineKeyboardButton(text="‹ أستمرار › ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="‹ مؤقتا ›", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="‹ ايقاف ›", callback_data=f"ADMIN Stop|{chat_id}")
         ],
         [
             InlineKeyboardButton(
-                text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=SUPPORT_CHAT,
+                text="「 الدعم 」", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
