854cdb59088a464ed386edfe397048a902647004
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 01:30:01 +0300

    Update play.py

--- AarohiX/utils/decorators/play.py
+++ AarohiX/utils/decorators/play.py
@@ -31,19 +31,7 @@ def PlayWrapper(command):
     async def wrapper(client, message):
         language = await get_lang(message.chat.id)
         _ = get_string(language)
-        if message.sender_chat:
-            upl = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="AnonymousAdmin",
-                        ),
-                    ]
-                ]
-            )
-            return await message.reply_text(_["general_3"], reply_markup=upl)
-
+        
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
