15d07ee2aa284db65e73225ffd7b24faa02a951a
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-18 20:47:11 +0300

    Update كيبورد.py

--- AarohiX/plugins/play/كيبورد.py
+++ AarohiX/plugins/play/كيبورد.py
@@ -15,12 +15,12 @@ developer_keyboard = ReplyKeyboardMarkup(
 )
 
 # دالة للرد على الرسائل الخاصة بالمطور
-@app.on_message(filters.user(OWNER_ID) & filters.private & filters.text)
+@app.on_message(user(OWNER_ID) & filters.private & filters.text)
 async def developer_message(client, message):
     # إرسال لوحة المفاتيح المخصصة للمطور
     await message.reply("أهلا بك! اختر إحدى الخيارات:", reply_markup=developer_keyboard)
 
 # دالة للرد على أمر start للمستخدمين العاديين
-@app.on_message(filters.command("/start") & ~filters.user(OWNER_ID))
+@app.on_message(command("/start") & ~filters.user(OWNER_ID))
 async def start(client, message):
     await message.reply("مرحبًا بك في بوتنا!")
