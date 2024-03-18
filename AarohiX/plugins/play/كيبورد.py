877d7fbf5744ded644c1932f26fafd555b35fb79
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-18 21:50:25 +0300

    Update كيبورد.py

--- AarohiX/plugins/play/كيبورد.py
+++ AarohiX/plugins/play/كيبورد.py
@@ -32,7 +32,7 @@ developer_keyboard = ReplyKeyboardMarkup([
 
 # دالة للاستجابة عند استخدام المطور لأمر /start
 @app.on_message(filters.command("/admin") & filters.user(OWNER_ID))
-async def admin_command(client, message):
+async def start_2(client, message):
     # إرسال لوحة المفاتيح المخصصة للمطور
     await message.reply("أهلا بك! اختر إحدى الخيارات:", reply_markup=developer_keyboard)
 
