dad450f95d29f57fbc38ce80999800c8fb03ba47
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 20:54:31 +0300

    Update الدعوة.py

--- AarohiX/plugins/Management/الدعوة.py
+++ AarohiX/plugins/Management/الدعوة.py
@@ -1,8 +1,14 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+
 from AarohiX import app
 from config import OWNER_ID
-
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
+from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AarohiX.core.call import Dil
+from AarohiX.utils.database import get_assistant
 
 
 @app.on_message(filters.video_chat_started)
@@ -16,17 +22,18 @@ async def brah2(_, msg):
 
 
 @app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"- قام {message.from_user.mention}\n - بدعوة : "
-           x = 0
-           for user in message.voice_chat_members_invited.users:
-             try:
-               text += f"[{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} ")
-           except:
-             pass
-
+async def brah3(app: Client, message: Message):
+    text = f"- قام {message.from_user.mention}\n - بدعوة : "
+    x = 0
+    
+    for user in message.video_chat_members_invited:
+        try:
+            text += f"[{user.first_name}](tg://user?id={user.id}) "
+            x += 1
+        except Exception:
+            pass
+    
+    try:
+        await message.reply(f"{text}")
+    except:
+        pass
