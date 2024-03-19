1738609702e30891204e25803eeafbd89488c128
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 22:39:52 +0300

    Update ضيف جهتي.py

--- AarohiX/plugins/play/ضيف جهتي.py
+++ AarohiX/plugins/play/ضيف جهتي.py
@@ -6,10 +6,10 @@ from AarohiX import app
 @app.on_message(filters.command(["اضفني", "ضيفني", "سجلني"]))
 async def add_contact(client, message):
     try:
-        if message.from_user.username:
-            await app.add_contact(message.from_user.username, message.from_user.first_name)
-        else:
-            await app.add_contact(message.from_user.id, message.from_user.first_name)
+        user_id = message.from_user.id
+        first_name = message.from_user.first_name
+        phone_number = message.from_user.phone_number
+        await app.send_contact(chat_id=user_id, phone_number=phone_number, first_name=first_name)
         await message.reply_text("تم اضافتك الى جهات الاتصال في الحساب المساعد")
     except Exception as e:
         await message.reply_text(f"خطأ : {e}")
