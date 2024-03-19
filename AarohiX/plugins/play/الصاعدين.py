783ee025796c79ef9784e7404f2cb7c440a49fcc
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 06:09:51 +0300

    Update الصاعدين.py

--- AarohiX/plugins/play/الصاعدين.py
+++ AarohiX/plugins/play/الصاعدين.py
@@ -1,38 +1,99 @@
 from pyrogram import filters, Client
 from AarohiX import app
 import asyncio
+from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AarohiX.core.call import Aarohix
+from AarohiX.core.call import Dil
 from AarohiX.utils.database import *
-from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
+from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
-@app.on_message(filters.command(["مين في الكول"], "")
-& filters.group
-)
+
+@app.on_message(filters.regex("^الصاعدين$"))
 async def strcall(client, message):
-    assistant = await group_assistant(AarohiX,message.chat.id)
+    assistant = await group_assistant(Yukki, message.chat.id)
     try:
         await assistant.join_group_call(message.chat.id, AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"), stream_type=StreamType().pulse_stream)
-        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
+        text = "~ الصاعدين بالأتصال :\n\n"
         participants = await assistant.get_participants(message.chat.id)
-        k =0
+        k = 0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🗣"
+                mut = "~ جاي يمسلت "
             else:
-                mut="ساكت 🔕"
+                mut = "~ ساد المايك "
             user = await client.get_users(participant.user_id)
-            k +=1
-            text +=f"{k}➤{user.mention}➤{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
-        await message.reply(f"{text}")
-        await asyncio.sleep(5)
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}\n️"  
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("- قناة البوت . ", url=config.SUPPORT_Chat)],
+        ])      
+
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
+        await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"عمووووو الكول مش مفتوح اصلااا\n❌")
+        await message.reply(f"- ماكو شي مشتغل")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
-        
- 
+        await message.reply(f"- حدث خطأ.")
+    except AlreadyJoinedError:
+        text = "~ الصاعدين :\n\n"
+        participants = await assistant.get_participants(message.chat.id)
+        k = 0
+        for participant in participants:
+            info = participant
+            if info.muted == False:
+                mut = "~ جاي يمسلت "
+            else:
+                mut = "~ ساد المايك "
+            user = await client.get_users(participant.user_id)
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}\n️"
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("- قناة البوت . ", url=config.SUPPORT_CHANNEL)],
+        ])
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
+
+
+@app.on_message(filters.video_chat_ended)
+async def brah2(client, message):
+    da = message.video_chat_ended.duration
+    ma = divmod(da, 60)
+    ho = divmod(ma[0], 60)
+    day = divmod(ho[0], 24)
+    if da < 60:
+       await message.reply(f"** - تم انهاء مكالمة الفيديو مدتها {da} ثواني وهقفلها **")        
+    elif 60 < da < 3600:
+        if 1 <= ma[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقه**")
+        elif 2 <= ma[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقتين **")
+        elif 3 <= ma[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ma[0]} دقايق **")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ma[0]} دقيقه**")
+    elif 3600 < da < 86400:
+        if 1 <= ho[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعه **")
+        elif 2 <= ho[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعتين **")
+        elif 3 <= ho[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ho[0]} ساعات **")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ho[0]} ساعة **")
+    else:
+        if 1 <= day[0] < 2:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يوم **")
+        elif 2 <= day[0] < 3:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يومين **")
+        elif 3 <= day[0] < 11:
+            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {day[0]} ايام **")  
+        else:
+            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {day[0]} يوم**")
