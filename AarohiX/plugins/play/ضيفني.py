75b53458b7f7731579c43cfd6916a895bf168c5a
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 00:08:55 +0300

    Update ضيفني.py

--- AarohiX/plugins/play/ضيفني.py
+++ AarohiX/plugins/play/ضيفني.py
@@ -1,14 +1,10 @@
-from strings.filters import command
+from pyrogram import Client, filters
 from AarohiX import app
-from AarohiX.core.userbot import assistants
 
-@app.on_message(command(["اضفني","ضيفني","سجلني"]))
-async def leave_one(client, message):
+@app.on_message(filters.command(["اضفني", "ضيفني", "سجلني"]))
+async def add_me(client, message):
     try:
-        if message.from_user.username:
-            await userbot.add_contact(message.from_user.username, message.from_user.first_name)
-        else:
-            await userbot.add_contact(message.from_user.id, message.from_user.first_name)
-        await message.reply_text("تم اضافتك الي جهات الاتصال في الحساب المساعد")
+        await app.add_contact(message.from_user.id, first_name=message.from_user.first_name)
+        await message.reply_text("تمت إضافتك إلى جهات الاتصال في الحساب المساعد")
     except Exception as e:
-        await message.reply_text(f"خطأ : {e}")
+        await message.reply_text(f"حدث خطأ: {e}")
