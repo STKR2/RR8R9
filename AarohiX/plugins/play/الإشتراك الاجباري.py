37ed5c949f2aeed66abe449aaf4a3bd45400b5f3
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 15:00:08 +0300

    Update الإشتراك الاجباري.py

--- AarohiX/plugins/play/الإشتراك الاجباري.py
+++ AarohiX/plugins/play/الإشتراك الاجباري.py
@@ -2,23 +2,24 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from AarohiX import app
+from config import CHANNEL_LINK
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/vvyvv6":  # Not compulsory
+    if not CHANNEL_LINK:
         return
     try:
         try:
-            await bot.get_chat_member("vvyvv6", msg.from_user.id)
+            await bot.get_chat_member(CHANNEL_LINK, msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/vvyvv6".isalpha():
-                link = "https://t.me/vvyvv6"
+            if CHANNEL_LINK.isalpha():
+                link = CHANNEL_LINK
             else:
-                chat_info = await bot.get_chat("vvyvv6")
+                chat_info = await bot.get_chat(CHANNEL_LINK)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"~︙اولا عليك  {msg.from_user.mention} \n~︙الأشتراك بقناة البوت \n~︙قناة البوت : @vvyvv6 .",
+                    f"~︙اولا عليك  {msg.from_user.mention} \n~︙الأشتراك بقناة البوت \n~︙قناة البوت : {CHANNEL_LINK}.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("< Team Freedom >", url=link)]
@@ -28,4 +29,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOIN chat @vvyvv6 !")
+        print(f"I m not admin in the MUST_JOIN chat {CHANNEL_LINK}!")
