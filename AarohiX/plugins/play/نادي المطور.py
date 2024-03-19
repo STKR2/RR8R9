b1a3b8790a883c615a5fced46b4271a67e07a556
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 22:26:49 +0300

    Create نادي المطور.py

---
+++ AarohiX/plugins/play/نادي المطور.py
@@ -0,0 +1,39 @@
+
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from YukkiMusic.utils.decorators import AdminActual
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InputMediaPhoto,
+    Message,
+)
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+
+@app.on_message(filters.command("المطور", [".", ""]) & filters.group)
+async def kstr(client: Client, message: Message):
+       chat = message.chat.id
+       gti = message.chat.title
+       link = await app.export_chat_invite_link(chat)
+       usr = await client.get_users(message.from_user.id)
+       chatusername = f"@{message.chat.username}"
+       user_id = message.from_user.id
+       user_ab = message.from_user.username
+       user_name = message.from_user.first_name
+       buttons = [[InlineKeyboardButton(gti, url=f"{link}")]]
+       reply_markup = InlineKeyboardMarkup(buttons)
+       
+       await app.send_message(-1001854546683, f"- قام {message.from_user.mention}\n- بمناداتك عزيزي المطور\n- ايديه {user_id}\n- يوزره @{user_ab}\n- ايدي القروب {message.chat.id}\n- يوزر القروب {chatusername}",
+       reply_markup=reply_markup,
+       )
+       await message.reply_text(
+        f"""- **ابشر ياعيوني ارسلت للمطور هيدخل الجروب ويشوف مشكلتك بأقرب وقت\n\n- تابع قناة البوت عشان تشوف التحديثات** -› [ 𝙎𝙊𝙐𝙍𝘾𝙀 𝙎𝙏𝘼𝙍 •](t.me/S0URCE_STAR)""", disable_web_page_preview=True     
+    )
+
