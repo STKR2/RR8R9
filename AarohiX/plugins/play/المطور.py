1476c786389d38f78c01a6b28f3530c77c330d0a
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 05:04:41 +0300

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -2,46 +2,49 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
 from AarohiX import app
-from config import SUPPORT_CHAT, OWNER_ID
+import config
 
 @app.on_message(
     command(["اوامر", "الاوامر"])
 )
 async def mmmezat(client, message):
     await message.reply_text(
-        f"""مرحبآ بك عزيزي {message.from_user.mention}في قسم مميزات سورس cr ميوزك\n
-        هنا تكتب الاوامر """,
+        f"""مرحبًا بك عزيزي {message.from_user.mention} في قسم مميزات سورس cr ميوزك\nهنا تكتب الاوامر """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "- المطور .", url=f"tg://openmessage?user_id={OWNER_ID}),
+                        "- المطور .", url=f"tg://openmessage?user_id={config.OWNER_ID}"
+                    ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "- مسح .", callback_data="close"),
+                        "- مسح .", callback_data="close"
+                    ),
                 ],
             ]
         ),
     )
 
 @app.on_message(
-    command(["المطور","السورس","المصنع"])
+    command(["المطور", "السورس", "المصنع"])
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/402c519808f75bd9b1803.jpg",
-        caption=f"""~ Team freedom \n~ Dav Source """,
+        photo="https://te.legra.ph/file/402c519808f75bd9b1803.jpg",
+        caption="~ Team freedom \n~ Dav Source",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "- مطور البوت .", url=f"tg://openmessage?user_id={OWNER_ID}),
+                        "- مطور البوت .", url=f"tg://openmessage?user_id={config.OWNER_ID}"
+                    ),
                 ],
                 [
-                   InlineKeyboardButton(
-                        "- قناة البوت . ", url=config.SUPPORT_CHAT),
-                ],       
+                    InlineKeyboardButton(
+                        "- قناة البوت . ", url=config.SUPPORT_CHAT
+                    ),
+                ],
             ]
         ),
     )
