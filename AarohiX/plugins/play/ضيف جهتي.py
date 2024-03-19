294032ea5317f849ecfbd8fe1e9ca24030ffaf5d
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 22:58:08 +0300

    Update ضيف جهتي.py

--- AarohiX/plugins/play/ضيف جهتي.py
+++ AarohiX/plugins/play/ضيف جهتي.py
@@ -1,15 +1,13 @@
-from strings.filters import command
-from AarohiX.core.call import Dil
 from pyrogram import Client, filters
 from AarohiX import app
 
-@app.on_message(filters.command(["اضفني", "ضيفني", "سجلني"]))
+@app.on_message(filters.command(["اضفني", "ضيفني"]))
 async def add_contact(client, message):
     try:
         user_id = message.from_user.id
         first_name = message.from_user.first_name
         phone_number = message.from_user.phone_number
         await app.send_contact(chat_id=user_id, phone_number=phone_number, first_name=first_name)
-        await message.reply_text("تم اضافتك الى جهات الاتصال في الحساب المساعد")
+        await message.reply_text("~ تمت اضافتك الى جهات حساب المساعد .")
     except Exception as e:
-        await message.reply_text(f"خطأ : {e}")
+        await message.reply_text(f"Error: {e}")
