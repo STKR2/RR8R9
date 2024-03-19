bacda07828d54bb0cdb35574a8cc80e4ed1d00c6
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 22:18:47 +0300

    Create ضيف جهتي.py

---
+++ AarohiX/plugins/play/ضيف جهتي.py
@@ -0,0 +1,15 @@
+from strings.filters import command
+from AarohiX.core.call import Dil
+from pyrogram import Client, filters
+
+
+@app.on_message(filters.command(["اضفني", "ضيفني", "سجلني"]))
+async def add_contact(client, message):
+    try:
+        if message.from_user.username:
+            await app.add_contact(message.from_user.username, message.from_user.first_name)
+        else:
+            await app.add_contact(message.from_user.id, message.from_user.first_name)
+        await message.reply_text("تم اضافتك الى جهات الاتصال في الحساب المساعد")
+    except Exception as e:
+        await message.reply_text(f"خطأ : {e}")
