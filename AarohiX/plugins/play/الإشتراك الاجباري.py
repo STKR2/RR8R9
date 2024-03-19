037cc816ed15df67a5a28141d7322bb5831e0f93
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 17:07:27 +0300

    Update الإشتراك الاجباري.py

--- AarohiX/plugins/play/الإشتراك الاجباري.py
+++ AarohiX/plugins/play/الإشتراك الاجباري.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from AarohiX import app
-from config import BOT_IMAGE, Muntazer
+from config import Muntazer
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -17,32 +17,16 @@ async def must_join_channel(app: Client, msg: Message):
             else:
                 chat_info = await app.get_chat(Muntazer)
                 link = chat_info.invite_link
-
             try:
-                if BOT_IMAGE:
-                    # إذا كانت الصورة متوفرة
-                    await msg.reply(
-                        f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
-                        reply_markup=InlineKeyboardMarkup([
-                            [InlineKeyboardButton("< Team Freedom >", url=link)]
-                        ])
-                    )
-                    await app.send_photo(
-                        chat_id=msg.chat.id,
-                        photo=BOT_IMAGE,
-                        caption=" ",
-                        reply_to_message_id=msg.message_id
-                    )
-                else:
-                    # إذا لم تكن الصورة متوفرة
-                    await msg.reply(
-                        f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
-                        reply_markup=InlineKeyboardMarkup([
-                            [InlineKeyboardButton("< Team Freedom >", url=link)]
-                        ])
-                    )
+                await msg.reply(
+                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("< Team Freedom >", url=link)]
+                    ])
+                )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat {Muntazer}!")
+        print(f"I m not admin in the MUST_JOIN chat {Muntazer}!")
