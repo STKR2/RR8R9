5f081386f70e4eda3b7c9e6398d4b7554aa51e26
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 14:27:14 +0300

    Create must_join.py

---
+++ AarohiX/plugins/play/must_join.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from AarohiX import app
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not "https://t.me/vvyvv6":  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member("vvyvv6", msg.from_user.id)
+        except UserNotParticipant:
+            if "https://t.me/vvyvv6".isalpha():
+                link = "https://t.me/vvyvv6"
+            else:
+                chat_info = await bot.get_chat("vvyvv6")
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @vvyvv6 .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("ᯓ 𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾 𝅘𝅥𝅯", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I m not admin in the MUST_JOIN chat @UI_VM !")
