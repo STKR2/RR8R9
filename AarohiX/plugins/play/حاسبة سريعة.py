272e37b6ac47371db9a06e78d160a2ec4bf6f29f
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 20:49:28 +0300

    Create حاسبة سريعة.py

---
+++ AarohiX/plugins/play/حاسبة سريعة.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from AarohiX import app
+from config import OWNER_ID
+
+@app.on_message(command("احسب", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("احسب ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"~ الناتج هو : {result}"
+    except:
+        response = "~ اكتب هكذا\n احسب 3 + 3 × 4"
+    message.reply(response)
