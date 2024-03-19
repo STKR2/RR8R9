98d5c35bf1aeb99c3027e22fdca9b05a7067086e
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-19 15:57:49 +0300

    Update الإشتراك الاجباري.py

--- AarohiX/plugins/play/الإشتراك الاجباري.py
+++ AarohiX/plugins/play/الإشتراك الاجباري.py
@@ -2,24 +2,24 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from AarohiX import app
-from config import muntazer
+from config import Muntazer
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not muntazer:
+    if not Muntazer:
         return
     try:
         try:
-            chat_id = await bot.resolve_peer(muntazer)
+            chat_id = await bot.resolve_peer(Muntazer)
             await bot.get_chat_member(chat_id,msg.from_user.id)
         except UserNotParticipant:
-            if muntazer.isalpha():
-                link = f"https://t.me/{muntazer}"
+            if Muntazer.isalpha():
+                link = f"https://t.me/{Muntazer}"
             else:
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"~︙اولا عليك  {msg.from_user.mention} \n~︙الأشتراك بقناة البوت \n~︙قناة البوت : {muntazer}.",
+                    f"~︙اولا عليك  {msg.from_user.mention} \n~︙الأشتراك بقناة البوت \n~︙قناة البوت : {Muntazer}.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("< Team Freedom >", url=link)]
@@ -29,4 +29,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOIN chat {muntazer}!")
+        print(f"I m not admin in the MUST_JOIN chat {Muntazer}!")
