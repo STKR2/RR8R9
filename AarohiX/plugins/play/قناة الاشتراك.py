e7985d46ec7b15b99089507a402e948313105a23
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 19:54:18 +0300

    Create قناة الاشتراك.py

---
+++ AarohiX/plugins/play/قناة الاشتراك.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from AarohiX import app
+from config import Muntazer
+
+@app.on_message(filters.private)
+async def must_join_channel(_, message):
+    if "‹ قناة الاشتراك ›" in message.text:
+        link = f"https://t.me/{Muntazer}"
+        await message.reply(
+            text="~ عزيزي المطور \n~ هذا هي قناة الاشتراك الاجباري @{Muntazer} .",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("freedom", url=link)]
+            ])
+        )
