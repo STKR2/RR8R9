dbfb04a2b157463803f6d9d17e63998e7c54f2fd
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 21:06:48 +0300

    Update restart.py

--- AarohiX/plugins/sudo/restart.py
+++ AarohiX/plugins/sudo/restart.py
@@ -26,7 +26,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(command(["‹ سجلات التشغيل ›", "السجلات", "getlogs"]) & (OWNER_ID))
+@app.on_message(command(["‹ سجلات التشغيل ›", "السجلات", "السجل"]) & (SUDOERS))
 @language
 async def log_(client, message, _):
     try:
