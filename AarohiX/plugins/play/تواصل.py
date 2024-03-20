d9666b67830977efb3591a28e0b23b0ef6c4b6a1
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 03:30:05 +0300

    Create تواصل.py

---
+++ AarohiX/plugins/play/تواصل.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+import config
+from AarohiX import app
+
+@app.on_message(filters.command("تفعيل_التواصل") & filters.user(config.OWNER_ID))
+def enable_communication(client, message):
+    message.reply_text("تم تفعيل التواصل مع الأعضاء")
+
+@app.on_message(filters.command("تعطيل_التواصل") & filters.user(config.OWNER_ID))
+def disable_communication(client, message):
+    message.reply_text("تم تعطيل التواصل مع الأعضاء")
+
+
+@app.on_message(~filters.command & filters.reply & filters.private)
+def reply_to_message(client, message):
+    replied_message = message.reply_to_message
+    message.reply_text(f"لقد ردت على الرسالة التي تم استقبالها: {replied_message.text}")
