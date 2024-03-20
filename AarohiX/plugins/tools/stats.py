430a8f9e9db5383c8188177446e9ed4fe9f053f5
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 06:49:42 +0300

    Update stats.py

--- AarohiX/plugins/tools/stats.py
+++ AarohiX/plugins/tools/stats.py
@@ -22,7 +22,7 @@ from config import BANNED_USERS
 @app.on_message(filters.command(["stats", "gstats"]) & ~BANNED_USERS)
 @language
 async def stats_global(client, message: Message, _):
-    upl = stats_buttons(_, not message.from_user.id in SUDOERS)
+    upl = stats_buttons(_, not message.from_user.id in OWNER_ID)
     await message.reply_photo(
         photo=config.STATS_IMG_URL,
         caption=_["gstats_2"].format(app.mention),
@@ -33,7 +33,7 @@ async def stats_global(client, message: Message, _):
 @app.on_callback_query(filters.regex("stats_back") & ~BANNED_USERS)
 @languageCB
 async def home_stats(client, CallbackQuery, _):
-    upl = stats_buttons(_, not CallbackQuery.from_user.id in SUDOERS)
+    upl = stats_buttons(_, not CallbackQuery.from_user.id in OWNER_ID)
     await CallbackQuery.edit_message_text(
         text=_["gstats_2"].format(app.mention),
         reply_markup=upl,
@@ -75,7 +75,7 @@ async def overall_stats(client, CallbackQuery, _):
 @app.on_callback_query(filters.regex("bot_stats_sudo"))
 @languageCB
 async def bot_stats(client, CallbackQuery, _):
-    if CallbackQuery.from_user.id not in SUDOERS:
+    if CallbackQuery.from_user.id not in OWNER_ID:
         return await CallbackQuery.answer(_["gstats_4"], show_alert=True)
     upl = back_stats_buttons(_)
     try:
