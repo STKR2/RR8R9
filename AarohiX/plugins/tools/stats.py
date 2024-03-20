8b5fcdb8473d8b38afb7811f63e4588ed4c68ad4
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-20 04:05:10 +0300

    Update stats.py

--- AarohiX/plugins/tools/stats.py
+++ AarohiX/plugins/tools/stats.py
@@ -150,7 +150,7 @@ async def bot_stats(client, CallbackQuery, _):
 
 
 
-@Client.on_message(filters.command(["《تنظيف》"], "") & SUDOERS)
+@app.on_message(filters.command(["《تنظيف》"], "") & SUDOERS)
 async def clean(client: Client, message):
     try:
         await message.delete()
@@ -186,7 +186,7 @@ def testspeed(m):
     return result
 
 
-@Client.on_message(filters.command(["《فحص سرعه البوت》"], "") & SUDOERS)
+@app.on_message(filters.command(["فحص سرعه البوت"], "") & SUDOERS)
 async def spedtest(client: Client, message):
     m = await message.reply_text("**» ʀᴜɴɴɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ...**")
     loop = asyncio.get_event_loop()
@@ -207,83 +207,4 @@ async def spedtest(client: Client, message):
         chat_id=message.chat.id, photo=result["share"], caption=output
     )
     await m.delete()
-
-
-@Client.on_message(filters.command(["《معلومات السيرفر》"], "") & SUDOERS)
-async def serverinfoo(client: Client, message):
-    sysrep = await message.reply_text(
-        f"ɢᴇᴛᴛɪɴɢ {MUSIC_BOT_NAME} sʏsᴛᴇᴍ sᴛᴀᴛs, ɪᴛ'ʟʟ ᴛᴀᴋᴇ ᴀ ᴡʜɪʟᴇ..."
-    )
-    try:
-        await message.delete()
-    except:
-        pass
-    sudoers = len(SUDOERS)
-    mod = len(ALL_MODULES)
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(socket.gethostname())
-    architecture = platform.machine()
-    processor = platform.processor()
-    mac_address = ":".join(re.findall("..", "%012x" % uuid.getnode()))
-    sp = platform.system()
-    ram = str(round(psutil.virtual_memory().total / (1024.0**3))) + " ɢʙ"
-    p_core = psutil.cpu_count(logical=False)
-    t_core = psutil.cpu_count(logical=True)
-    try:
-        cpu_freq = psutil.cpu_freq().current
-        if cpu_freq >= 1000:
-            cpu_freq = f"{round(cpu_freq / 1000, 2)}ɢʜᴢ"
-        else:
-            cpu_freq = f"{round(cpu_freq, 2)}ᴍʜᴢ"
-    except:
-        cpu_freq = "ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ"
-    hdd = psutil.disk_usage("/")
-    total = hdd.total / (1024.0**3)
-    total = str(total)
-    used = hdd.used / (1024.0**3)
-    used = str(used)
-    free = hdd.free / (1024.0**3)
-    free = str(free)
-    platform_release = platform.release()
-    platform_version = platform.version()
-
-    await sysrep.edit_text(
-        f"""
-➻ <u>**{MUSIC_BOT_NAME} sʏsᴛᴇᴍ sᴛᴀᴛs**</u>
-
-**ᴩʏᴛʜᴏɴ :** {pyver.split()[0]}
-**ᴩʏʀᴏɢʀᴀᴍ :** {pyrover}
-**ᴩʏ-ᴛɢᴄᴀʟʟs :** {pytgver}
-**sᴜᴅᴏᴇʀs :** `{sudoers}`
-**ᴍᴏᴅᴜʟᴇs :** `{mod}`
-
-**ɪᴘ :** {ip_address}
-**ᴍᴀᴄ :** {mac_address}
-**ʜᴏsᴛɴᴀᴍᴇ :** {hostname}
-**ᴘʟᴀᴛғᴏʀᴍ :** {sp}
-**ᴘʀᴏᴄᴇssᴏʀ :** {processor}
-**ᴀʀᴄʜɪᴛᴇᴄᴛᴜʀᴇ :** {architecture}
-**ᴘʟᴀᴛғᴏʀᴍ ʀᴇʟᴇᴀsᴇ :** {platform_release}
-**ᴘʟᴀᴛғᴏʀᴍ ᴠᴇʀsɪᴏɴ :** {platform_version}
-
-        <b><u>sᴛᴏʀᴀɢᴇ</b><u/>
-**ᴀᴠᴀɪʟᴀʙʟᴇ :** {total[:4]} ɢɪʙ
-**ᴜsᴇᴅ :** {used[:4]} ɢɪʙ
-**ғʀᴇᴇ :** {free[:4]} ɢɪʙ
-
-**ʀᴀᴍ :** {ram}
-**ᴩʜʏsɪᴄᴀʟ ᴄᴏʀᴇs :** {p_core}
-**ᴛᴏᴛᴀʟ ᴄᴏʀᴇs :** {t_core}
-**ᴄᴩᴜ ғʀᴇǫᴜᴇɴᴄʏ :** {cpu_freq}""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="ᴄʟᴏsᴇ",
-                        callback_data=f"forceclose abc|{message.from_user.id}",
-                    ),
-                ]
-            ]
-        ),
-    )
 
