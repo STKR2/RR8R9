778e732739f202f12ed31bd8786b1ee69111c7c0
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 01:03:04 +0300

    Update restart.py

--- AarohiX/plugins/sudo/restart.py
+++ AarohiX/plugins/sudo/restart.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import socket
 from datetime import datetime
-from strings.filters import command
+
 import urllib3
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
@@ -22,11 +22,12 @@ from AarohiX.utils.pastebin import DilBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(command(["getlog", "logs", "السجلات"]) & SUDOERS)
+@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & SUDOERS)
 @language
 async def log_(client, message, _):
     try:
@@ -35,7 +36,7 @@ async def log_(client, message, _):
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(command(["تحديث", "حدث"]) & SUDOERS)
+@app.on_message(filters.command(["update", "gitpull"]) & SUDOERS)
 @language
 async def update_(client, message, _):
     if await is_heroku():
@@ -64,12 +65,12 @@ async def update_(client, message, _):
     )
     for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
         updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>➣ يوجد تحديث جديد للبوت</b>\n➣ قم بتحديث بوتك الان\n\n<b>➣ التحديثات :</b>\n"
+    _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
     _final_updates_ = _update_response_ + updates
     if len(_final_updates_) > 4096:
-        url = await ZelzalyBin(updates)
+        url = await DilBin(updates)
         nrs = await response.edit(
-            f"<b>➣ يوجد تحديث جديد للبوت</b>\n➣ قم بتحديث بوتك الان\n\n<b>➣ التحديثات :</b>\n\n<a href={url}>اضغط هنا لرؤية التحديثات</a>"
+            f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
         )
     else:
         nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
@@ -109,15 +110,15 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(command(["اعاده تشغيل"]) & SUDOERS)
+@app.on_message(filters.command(["restart"]) & SUDOERS)
 async def restart_(_, message):
-    response = await message.reply_text("- جـارِ إعـادة التشغيـل ...")
+    response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
     for x in ac_chats:
         try:
             await app.send_message(
                 chat_id=int(x),
-                text=f"» {app.mention} في وضـع اعـادة التشغيـل ...\n\n» انتظـر 20-15 دقيقـه ⏳. . .",
+                text=f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
             )
             await remove_active_chat(x)
             await remove_active_video_chat(x)
@@ -131,6 +132,6 @@ async def restart_(_, message):
     except:
         pass
     await response.edit_text(
-        "» جـارِ اعـادة تشغيـل البـوت ...\n» انتظـر عـدة دقـائـق ⏳\n» حتـى يعمـل البـوت ☑️..."
+        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
     )
     os.system(f"kill -9 {os.getpid()} && bash start")
