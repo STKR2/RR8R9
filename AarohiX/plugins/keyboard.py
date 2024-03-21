2d5f0a59c98f2275bc75dcf8d87e512b2191c0cb
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 03:46:28 +0300

    Create keyboard.py

---
+++ AarohiX/plugins/keyboard.py
@@ -0,0 +1,30 @@
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
+from config import OWNER_ID
+from AarohiX import app
+
+# تعريف الكيبورد الخاص بالمطور
+admin_keyboard = ReplyKeyboardMarkup([
+    [('تفعيل_التواصل'), ('/broadcast'), ('حاله التواصل')],
+    [('ضع قناة الاشتراك'), ('حذف قناه الاشتراك')],
+    [('اذاعه للمطورين'), ('اذاعه للاساسيين'), ('اذاعه للقنوات')],
+    [('اذاعه للكل'), ('توجيه للكل')],
+    [('توجيه للمستخدمين'), ('توجيه للجروبات'), ('توجيه للقنوات')],
+    [('اخفاء الكيبورد ⚒️')]],
+    resize_keyboard=True,
+)
+
+# دالة للتعامل مع الأمر /admin
+@app.on_message(filters.command("admin") & filters.user(OWNER_ID))
+async def admin(client, message):
+    if message.from_user.id == OWNER_ID:
+        await message.reply("لوحة الكيبورد الخاصة بالمطور", reply_markup=admin_keyboard)
+    else:
+        pass
+
+# دالة للتعامل مع الأوامر المرتبطة بباقي السورس
+@app.on_message(filters.text & ~filters.command("admin") & filters.user(OWNER_ID))
+async def handle_commands(client, message):
+    # ادخل هنا التحكم بالأوامر المرتبطة بباقي السورس
+    pass
