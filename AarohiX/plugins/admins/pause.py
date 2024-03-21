cbc7e32969c08e4bcc79e2366ba6fac135b91cd0
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 05:26:11 +0300

    Update pause.py

--- AarohiX/plugins/admins/pause.py
+++ AarohiX/plugins/admins/pause.py
@@ -6,10 +6,9 @@ from AarohiX.core.call import Dil
 from AarohiX.utils.database import is_music_playing, music_off
 from AarohiX.utils.decorators import AdminRightsCheck
 from AarohiX.utils.inline import close_markup
-from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["مؤقتا", "cpause"]))
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
