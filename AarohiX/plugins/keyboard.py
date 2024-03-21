e597b87236a94a35f3645d95f10ba8e90853a707
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 06:19:51 +0300

    Update keyboard.py

--- AarohiX/plugins/keyboard.py
+++ AarohiX/plugins/keyboard.py
@@ -1,19 +1,15 @@
 import pyrogram
-from pyrogram import Client, filters
+from pyrogram import Client, Filters
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
 from config import OWNER_ID
 from AarohiX import app
 
-# تعريف الكيبورد الخاص بالمطور
-admin_keyboard = ReplyKeyboardMarkup([
-    ['تفعيل التواصل', '/broadcast', 'حالة التواصل'],
-    ['ضع قناة الاشتراك', 'حذف قناة الاشتراك'],
-    ['اذاعة للمطورين', 'اذاعة للأساسيين', 'اذاعة للقنوات'],
-    ['اذاعة للكل', 'توجيه للكل'],
-    ['توجيه للمستخدمين', 'توجيه للجروبات', 'توجيه للقنوات'],
-    ['اخفاء الكيبورد ⚒️']
-], resize_keyboard=True)
-
-@app.on_message(filters.command("adm") & filters.user(OWNER_ID))
-async def show_admin_keyboard(client, message):
-    await message.reply("لوحة المفاتيح الخاصة بالمطور", reply_markup=admin_keyboard)
+@app.on_message(Filters.command("muntazer") & Filters.user(OWNER_ID))
+def show_keyboard(client, message):
+    keyboard = ReplyKeyboardMarkup(
+        [
+            [KeyboardButton("Option 1"), KeyboardButton("Option 2")],
+            [KeyboardButton("Option 3"), KeyboardButton("Option 4")]
+        ]
+    )
+    message.reply_text('Choose an option:', reply_markup=keyboard)
