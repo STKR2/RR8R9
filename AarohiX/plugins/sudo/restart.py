38d65292ce1569a79420fb2f539f1db95634998d
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 15:33:08 +0300

    Update restart.py

--- AarohiX/plugins/sudo/restart.py
+++ AarohiX/plugins/sudo/restart.py
@@ -28,7 +28,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & filters.user(OWNER_ID))
+@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & filters.group (OWNER_ID))
 @language
 async def log_(client, message, _):
     try:
@@ -37,7 +37,7 @@ async def log_(client, message, _):
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(filters.command(["update", "gitpull"])  & filters.user(OWNER_ID))
+@app.on_message(filters.command(["update", "gitpull"])  & filters.group (OWNER_ID))
 @language
 async def update_(client, message, _):
     if await is_heroku():
@@ -111,7 +111,7 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(filters.command(["restart"])  & filters.user(OWNER_ID))
+@app.on_message(filters.command(["restart"])  & filters.group (OWNER_ID))
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
