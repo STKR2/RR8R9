80c42a6230e8c95c27a72dd902a9c1c690d07cdd
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 06:22:42 +0300

    Update keyboard.py

--- AarohiX/plugins/keyboard.py
+++ AarohiX/plugins/keyboard.py
@@ -4,12 +4,22 @@ from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
 from config import OWNER_ID
 from AarohiX import app
 
-@app.on_message(Filters.command("muntazer") & Filters.user(OWNER_ID))
-def show_keyboard(client, message):
-    keyboard = ReplyKeyboardMarkup(
-        [
-            [KeyboardButton("Option 1"), KeyboardButton("Option 2")],
-            [KeyboardButton("Option 3"), KeyboardButton("Option 4")]
-        ]
-    )
-    message.reply_text('Choose an option:', reply_markup=keyboard)
+admin_keyboard = ReplyKeyboardMarkup([
+    ['تفعيل التواصل', '/broadcast', 'حالة التواصل'],
+    ['ضع قناة الاشتراك', 'حذف قناة الاشتراك'],
+    ['اذاعة للمطورين', 'اذاعة للأساسيين', 'اذاعة للقنوات'],
+    ['اذاعة للكل', 'توجيه للكل'],
+    ['توجيه للمستخدمين', 'توجيه للجروبات', 'توجيه للقنوات'],
+    ['اخفاء الكيبورد ⚒️']
+], resize_keyboard=True)
+
+# دالة للتعامل مع أمر /admin
+@app.on_message(filters.command("admin") & filters.user(OWNER_ID))
+async def admin(client, message):
+    await message.reply("لوحة المفاتيح الخاصة بالمطور", reply_markup=admin_keyboard)
+
+# دالة للتعامل مع الأوامر الأخرى
+@app.on_message(filters.text & ~filters.command("admin") & filters.user(OWNER_ID))
+async def handle_commands(client, message):
+    # أدخل الكود الخاص بمعالجة الأوامر الأخرى هنا
+    pass
