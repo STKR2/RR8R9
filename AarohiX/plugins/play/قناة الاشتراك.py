60f2a764e3aefadeb0d6502b43bc81b6b9353cd1
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-21 06:09:21 +0300

    Update قناة الاشتراك.py

--- AarohiX/plugins/play/قناة الاشتراك.py
+++ AarohiX/plugins/play/قناة الاشتراك.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AarohiX import app
-from config import Muntazer
+from config import Muntazer, OWNER_ID
 
-@app.on_message(filters.private)
+@app.on_message(filters.private & filters.user(OWNER_ID))
 async def must_join_channel(_, message):
     if "‹ قناة الاشتراك ›" in message.text:
         link = f"https://t.me/{Muntazer}"
