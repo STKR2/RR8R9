fe51afa43d5d7217eb70d68d3f9cac0c8e1d4927
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-22 17:23:24 +0300

    Update تواصل.py

--- AarohiX/plugins/play/تواصل.py
+++ AarohiX/plugins/play/تواصل.py
@@ -10,10 +10,15 @@ from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 @app.on_message(filters.text & (filters.channel | filters.private))            
 async def hhhki(client: Client, message: Message):
-       msg = message.text
-       usr = await client.get_chat(message.from_user.id)
-       name = usr.first_name
-       usr_id = message.from_user.id
-       mention = message.from_user.mention
-       await app.send_message(OWNER_ID, f"- قام {mention} \n\n- بارسال رسالة للبوت \n\n- {msg}")
-
+    msg = message.text
+    if message.from_user is not None:
+        usr = await client.get_chat(message.from_user.id)
+        name = usr.first_name
+        usr_id = message.from_user.id
+        mention = message.from_user.mention
+        if message.from_user.id == OWNER_ID:  # Check if the user is the owner
+            await app.send_message(OWNER_ID, f"- قام {mention} \n\n- بارسال رسالة للبوت \n\n- {msg}")
+        else:
+            await message.reply("لا يمكنك الرد على الرسائل.")
+    else:
+        print("Received message from unknown user.")هذا التعديل يتحقق
