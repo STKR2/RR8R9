1c7a4c08109ff72921a09f9c00932bc0681b289c
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-22 16:51:58 +0300

    Create تواصل.py

---
+++ AarohiX/plugins/play/تواصل.py
@@ -0,0 +1,19 @@
+#heloo 
+
+import asyncio
+from pyrogram import Client, filters
+from strings.filters import command
+from config import OWNER_ID
+from pyrogram.types import Message , ReplyKeyboardMarkup , KeyboardButton
+from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(filters.text & (filters.channel | filters.private))            
+async def hhhki(client: Client, message: Message):
+       msg = message.text
+       usr = await client.get_chat(message.from_user.id)
+       name = usr.first_name
+       usr_id = message.from_user.id
+       mention = message.from_user.mention
+       await app.send_message(OWNER_ID, f"- قام {mention} \n\n- بارسال رسالة للبوت \n\n- {msg}")
+
