36220d427c85aea8b28a1b583e7b2f09efaeabad
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-22 20:28:31 +0300

    Update restart.py

--- AarohiX/plugins/sudo/restart.py
+++ AarohiX/plugins/sudo/restart.py
@@ -12,7 +12,7 @@ import urllib3
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
-
+from strings.filters import command
 import config
 from AarohiX import app
 from AarohiX.misc import HAPP, SUDOERS, XCB
@@ -31,7 +31,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["السجلات"]) & filters.user(OWNER_ID))
+@app.on_message(command(["السجلات"]) & filters.user(OWNER_ID))
 @language
 async def log_(client, message, _):
     try:
@@ -40,7 +40,7 @@ async def log_(client, message, _):
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(filters.command(["تحديث"]) & filters.private & filters.user(OWNER_ID))
+@app.on_message(command(["تحديث"]) & filters.private & filters.user(OWNER_ID))
 @language
 async def update_(client, message, _):
     if await is_heroku():
@@ -115,7 +115,7 @@ async def update_(client, message, _):
 
 
 
-@app.on_message(filters.command(["اعادة تشغيل"]) & filters.private & filters.user(OWNER_ID))
+@app.on_message(command(["اعادة تشغيل"]) & filters.private & filters.user(OWNER_ID))
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
