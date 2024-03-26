a94ad53e84074ee1984d0d1f8d6518b349889842
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-26 20:10:52 +0300

    Update active.py

--- AarohiX/plugins/tools/active.py
+++ AarohiX/plugins/tools/active.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
-
+from strings.filters import command
 from AarohiX import app
 from AarohiX.misc import SUDOERS
 from AarohiX.utils.database import (
@@ -12,9 +12,9 @@ from AarohiX.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
+@app.on_message(command(["الاتصالات", "activevoice"]) & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("~ جاري البحث عن اتصالات .")
     served_chats = await get_active_chats()
     text = ""
     j = 0
@@ -36,17 +36,17 @@ async def activevc(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"~ لاتوجد اتصالات الإن {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
+            f"<b>~ ها هي الأتصالات النشطة حاليا :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
+@app.on_message(command(["الفيديوات", "activevideo"]) & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("~ جاري البحث عن فيديوات متوفرة .")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
@@ -68,9 +68,9 @@ async def activevi_(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"~ لاتوجد فيديوات الأن  {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
+            f"<b>~ هذا هي قائمة الفيديوهات النشطة :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
