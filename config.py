449b2be56249da1fb89c2992f7d4f1baaa39b36a
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-01 15:22:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "bot")
 
-Muntazer = getenv("muntazer", "")
+Muntazer = getenv("muntazer", "vvyvv6")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
