18af5ccdbb2e75585ef035b32ef5019e3aac53de
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-07 21:20:39 +0300

    Update autoleave.py

--- AarohiX/plugins/misc/autoleave.py
+++ AarohiX/plugins/misc/autoleave.py
@@ -65,7 +65,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "~ الاتصال فارغ ، تم نزول حساب المساعد .",
                     )
                 except:
                     continue
