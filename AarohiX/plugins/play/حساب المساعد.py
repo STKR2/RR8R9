d1c3706fdedbfca2b4ef47cce89f8c972e7ff70d
Wa <122730917+STKR2@users.noreply.github.com>
2024-04-20 11:29:34 +0300

    Create حساب المساعد.py

---
+++ AarohiX/plugins/play/حساب المساعد.py
@@ -0,0 +1,19 @@
+from pyrogram import filters, Client
+from AarohiX import app
+import asyncio
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from AarohiX.core.call import Dil
+from AarohiX.utils.database import *
+from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError
+
+@app.on_message(filters.regex("اسم المساعد"))
+async def tom_name(client, message):
+    assistant = await group_assistant(Dil, message.chat.id)
+    await message.reply("ارسل اسم المساعد الجديد:")
+    try:
+        new_name = await client.ask(message.chat.id, "اكتب اسم المساعد الجديد:")
+        await assistant.update_profile(first_name=new_name)
+        await message.reply(f"تم تغيير اسم المساعد الى {new_name}")
+    except Exception as e:
+        await message.reply("حدث خطأ اثناء تغيير اسم المساعد!")
